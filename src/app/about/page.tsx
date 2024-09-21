46b423aa47f3267f763bee7c18e1d170debb4ac9
Qui Nguyen <qui@gmail.com>
2024-09-21 15:00:44 -0400

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti animi
+        itaque ut ea. Praesentium expedita illum explicabo nobis sunt sit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptatum
+        doloribus, voluptas cum impedit similique quibusdam pariatur expedita
+        ipsa hic aut nobis unde consequatur sit provident, nihil neque. Quod
+        veritatis sit molestiae voluptate, aut corrupti reprehenderit eius amet
+        fugiat minus, tempore quos consectetur blanditiis ut dignissimos tempora
+        et iure saepe. Corrupti tempora sequi dignissimos distinctio. Culpa, cum
+        accusamus doloremque facere eum magnam id deleniti nihil, voluptatem
+        excepturi ipsa, rem inventore. Perferendis expedita aspernatur
+        blanditiis! Qui sit magni officiis aliquam. Quae iure quisquam
+        voluptatum voluptatem, eum culpa aliquam dolorum laborum delectus, earum
+        porro magni sequi exercitationem molestias temporibus est consequatur
+        quos!
+      </p>
+    </main>
+  );
+}
