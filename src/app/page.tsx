6e2eb24efd8f5733db6656e86dd7da433f3e33f2
Qui Nguyen <qui@gmail.com>
2024-09-22 15:12:20 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import PostCard from "./components/PostCard";
 
-export default function Home() {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+const fetchPosts = async (): Promise<Post[]> => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/1/posts");
+  if (!res.ok) {
+    console.log("could not fetch the posts");
+  }
+  return res.json();
+};
+
+export default async function Home() {
+  const posts = await fetchPosts();
   return (
     <main>
-      <PostCard title="some title" author="Qui Nguyen" />
+      <h2>Home</h2>
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </main>
   );
 }
